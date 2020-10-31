8a39eeb1f38cff9e5f33068948d1d1187935a82c
Paulo Henrique Calletti <73306576+PauloCalletti@users.noreply.github.com>
2020-10-30 21:48:01 -0300

    Add files via upload

---
+++ if_ex01.js
@@ -0,0 +1,11 @@
+const MEDIA = 60
+
+let notaEstudante = 22
+  if (notaEstudante >= MEDIA) {
+    console.log("Aprovado")
+  } /*else if (notaEstudante >= 90) {
+    cosole.log("Para Béns")
+  } */else {
+    console.log("Reprovado")
+  }
+    
