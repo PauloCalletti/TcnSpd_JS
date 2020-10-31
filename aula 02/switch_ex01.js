b36ed7b97da67c6823d081315411d496d6b38d39
Paulo Henrique Calletti <73306576+PauloCalletti@users.noreply.github.com>
2020-10-30 21:50:39 -0300

    Add files via upload

---
+++ aula 02/switch_ex01.js
@@ -0,0 +1,20 @@
+/*Nota passada */
+let nota = "B";
+
+/* Instruções par o Switch */
+  switch(nota) {
+    case "A": 
+      console.log("Aluno Aprovado com A");
+    break; 
+    case "B": 
+      console.log("Aluno Aprovado com B");
+    break; 
+    case "C": 
+      console.log("Aluno Aprovado com C");
+    break; 
+    case "D": 
+      console.log("Aluno Aprovado com D");
+    break;
+    default: 
+      console.log("Aluno Reprovado");  
+  } 
